1f22ee8e13aba6e4df87c118ec842b99be364083
loica <loicarchimbaud@gmail.com>
2021-11-10 16:19:33 +0100

    Mise en place de l'architecture

---
+++ src/main/java/com/ecam/atsnum/AtsnumApplication.java
@@ -0,0 +1,13 @@
+package com.ecam.atsnum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsnumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsnumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecam/atsnum/AtsnumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecam.atsnum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsnumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

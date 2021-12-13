0689c2df1ba5474b8fee0da3475989d3d0c29073
Loic Archimbaud <loic.arhcimbaud@ecam.fr>
2021-12-13 09:03:28 +0100

    passage 1.0.2

--- src/main/resources/db/migration/V2.1.3__Insert_table_capteur_value.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO capteur_value(capteur_value, capteur_id,date_releve,machine_id) VALUES (10.5,21,'2021-11-16 17:00:00',1);
-INSERT INTO capteur_value(capteur_value, capteur_id,date_releve,machine_id) VALUES (10.5,22,'2021-11-16 17:00:00',1);
-INSERT INTO capteur_value(capteur_value, capteur_id,date_releve,machine_id) VALUES (10.5,23,'2021-11-16 17:00:00',1);
-INSERT INTO capteur_value(capteur_value, capteur_id,date_releve,machine_id) VALUES (10.5,24,'2021-11-16 17:00:00',1);
-INSERT INTO capteur_value(capteur_value, capteur_id,date_releve,machine_id) VALUES (10.5,26,'2021-11-16 17:00:00',1);
-INSERT INTO capteur_value(capteur_value, capteur_id,date_releve,machine_id) VALUES (10.5,27,'2021-11-16 17:00:00',1);
-INSERT INTO capteur_value(capteur_value, capteur_id,date_releve,machine_id) VALUES (10.5,29,'2021-11-16 17:00:00',1);
-
-
